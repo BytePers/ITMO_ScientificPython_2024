20921e514b1e0e480e81b0afb0cfe25cc132312c
BytePers <ratkevich.ilya@list.ru>
2024-03-24 19:36:51 +0300

    HW2_1.py file has been created. This is a file for retrieving ID information from Ensembl and UniProt databases via API

---
+++ HW2/HW2_1.py
@@ -0,0 +1,77 @@
+#FIRST TASK
+import re
+
+import requests, sys, json
+
+#Function 1 “get_<db_name>” that accepts list of IDs and outputs API request.
+def get_Uniprot(accessions_list: list):
+  return requests.get("https://rest.uniprot.org/uniprotkb/accessions", params={'accessions': accessions_list})
+
+#Function 2 “parse_response_<db_name>” that accepts response and outputs parsed data.
+def parse_response_Uniprot(resp = None):
+    resp = resp.json()
+    resp = resp["results"]
+    output = {}
+    for val in resp:
+        acc = val['primaryAccession']
+        species = val['organism']['scientificName']
+        gene = val['genes']
+        seq = val['sequence']
+        output[acc] = {'organism':species, 'geneInfo':gene, 'sequenceInfo':seq, 'type':'protein'}
+
+    return output
+
+#Function 1 “get_<db_name>” that accepts list of IDs and outputs API request.
+def get_ENSEMBL(accessions_list):
+
+    json_ids = json.dumps({"ids": accessions_list})
+    print(json_ids)
+    server = "https://rest.ensembl.org"
+    ext = "/lookup/id"
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    r = requests.post(server + ext, headers=headers, data=f'{json_ids}')
+
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+
+    decoded = r.json()
+    return decoded
+
+#Function 2 “parse_response_<db_name>” that accepts response and outputs parsed data.
+def parse_response_ENSEMBL(decoded):
+    output = {}
+    for val in decoded:
+        acc = decoded[val]['id']
+        species = decoded[val]['species']
+        gene = decoded[val]['description']
+        type = decoded[val]["object_type"]
+        output[acc] = {'organism':species, 'geneInfo':gene, 'type':type}
+
+    return output
+
+#SECOND TASK
+#Function 3
+def parse_ID(ID_list):
+
+    for ID in ID_list:
+        if not re.search(r'ENS+[A-Z0-9]{11}', 'r' + ID):
+            break
+        if ID_list.index(ID) == len(ID_list)-1:
+            return parse_response_ENSEMBL(get_ENSEMBL(accessions_list))
+
+    for ID in ID_list:
+        if not re.search(r'\w{6}', 'r' + ID):
+            break
+        if ID_list.index(ID) == len(ID_list) - 1:
+            return parse_response_Uniprot(get_Uniprot(accessions_list))
+
+    return 'Check the correctness of the entered identifiers'
+
+    #Checks the IDs for the regular expression match.
+    #Calls the get and parse functions of corresponding databases.
+    # Outputs the parsed data.
+
+
+accessions_list = list(input('Enter the accession IDs separated by spaces:').split())
+print(parse_ID(accessions_list))
